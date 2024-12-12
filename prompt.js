e56e2765395a705fb82ec660eb50ae19adc029b3
abderrahimrekkas <rekkasabderrahim8@gmail.com>
2024-12-12 14:29:08 +0100

    le code terminer

--- prompt.js
+++ prompt.js
@@ -1,23 +1,28 @@
 const prompt = require('prompt-sync')();
 for (let i = 0; i < 1000; i++) {
     const operateur = prompt("Entrez un operateur (+, -, *, /, ^, >) : ");
-    const num1 = prompt("Entrez un nombre : ");
-    const num2 = prompt("Entrez un autre nombre : ");
-    
+
         switch (operateur) {
+            
             case '+':
-                console.log(`resultat : ${num1 + num2}`);
+                 num1 = prompt("Entrez un nombre : ");
+                 num2 = prompt("Entrez un autre nombre : ");
+                 bom=parseFloat(num1)+parseFloat(num2)
+                 console.log(`resultat :${bom} `);
                 break;
-        
-            case '-':
-                console.log(`resultat : ${num1 - num2}`);
-                break;
-        
-            case '':
+            case'-':
+                 num1 = prompt("Entrez un nombre : ");
+                 num2 = prompt("Entrez un autre nombre : ");
+                 console.log(`resultat :${num1 - num2}`);
+                 break;
+            case '*':
+                num1 = prompt("Entrez un nombre : ");
+                num2 = prompt("Entrez un autre nombre : ");
                 console.log(`resultat : ${num1 * num2}`);
                 break;
-        
             case '/':
+                 num1 = prompt("Entrez un nombre : ");
+                 num2 = prompt("Entrez un autre nombre : ");
                 if (num2 === 0) {
                     console.log("Erreur wow ");
                 } else {
@@ -26,18 +31,20 @@ for (let i = 0; i < 1000; i++) {
                 break;
         
             case '^':
+                 num1 = prompt("Entrez un nombre : ");
+                num2 = prompt("Entrez un autre nombre : ");
                 console.log(`resultat : ${Math.pow(num1, num2)}`);
                 break;
         
             case '>':
+                num1 = prompt("Entrez un nombre : ");
                 if(num1 < 0){
                     console.log("Erreur");
                 }else {
-                    console.log(`resultat : ${Math.sqrt(num1,num2)}`);
+                    console.log(`resultat : ${Math.sqrt(num1)}`);
                 }
                 break;
         
-    
         default:
             console.log("Opérateur invalide !");
         }
